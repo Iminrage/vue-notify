afc759c027c7062b34c240a14943ac0c2917eae1
folkaras <efolkaras@gmail.com>
2020-01-17 18:33:41 +0300

    add notify route

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,18 @@ import Router from "vue-router";
 Vue.use(Router);
 
 import Home from "@/pages/Home.vue";
+import Notify from "@/pages/Notify.vue";
 export default new Router({
   routes: [
     {
       path: "/",
       name: "home",
       component: Home
-    }
+		},
+		{
+			path: "/notify",
+			name: "notify",
+			component: Notify
+		}
   ]
 });
