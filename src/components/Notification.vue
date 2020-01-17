40b076e76c8b1ee43aeec68ac7028ea48a09f695
folkaras <efolkaras@gmail.com>
2020-01-17 18:35:10 +0300

    add notify table

---
+++ src/components/Notification.vue
@@ -0,0 +1,20 @@
+<template>
+  <table>
+    <transition-group name="notify" tag="tbody">
+      <tr v-for="(message, index) in messages" :key="index">
+        <td>{{ message.title }} {{ message.text }}</td>
+      </tr>
+    </transition-group>
+  </table>
+</template>
+<script>
+export default {
+  name: "Notification",
+  props: {
+    messages: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
