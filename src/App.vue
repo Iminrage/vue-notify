0cd1bb74c775c8dc5eeb4789c8b9a457990e8728
folkaras <efolkaras@gmail.com>
2020-01-17 18:33:31 +0300

    add link to notify

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-		<nav>
-			<router-link to="/">Home</router-link>
-		</nav>
-		<router-view/>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/notify">Notify</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-  }
-}
+  name: "app",
+  components: {}
+};
 </script>
 
